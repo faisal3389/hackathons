3fb1fb270910da20e7ab93eca9bdaee379715cdf
Aditya Gangipamula <aditya.gangipamula@capillarytech.com>
2017-12-21 17:48:17 +0530

    observable observer wiring

--- rxStreams.js
+++ rxStreams.js
@@ -1,17 +1,33 @@
 var Rx = require('rxjs/Rx');
 
-var obs = Rx.Observable.of(1,2,3);
+var obs = Rx.Observable.of(4,7,6, 1, 9, 2,3);
 
 
-var observer = {};
 
-observer.next = function(o) {
-  console.log(o);
+function newObserver() {
+  var observer = { arr: [] };
+
+
+
+  observer.next = function(o) {
+    var boolMap = this.arr.map(x => x > o);
+    var index = boolMap.indexOf(true);
+    index = index === -1 ? this.arr.length: index;
+    this.arr.splice(index, 0, o);
+    console.log('index:' + index);
+    // this.arr.push(o);
+    console.log(o);
+
+    console.log(this.arr);
+  }
+
+
+  observer.complete = function(o) {
+    console.log('completed' + this.arr);
+  }
+
+  return observer;
 }
 
-
-observer.complete = function(o) {
-  console.log('completed');
-}
-
+var observer = newObserver();
 obs.subscribe(observer);
