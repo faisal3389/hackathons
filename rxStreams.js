5aa681aa645d7ed2d79e8b29ad842109da776944
Aditya Gangipamula <aditya.gangipamula@capillarytech.com>
2017-12-21 19:46:20 +0530

    better code org and comments

--- rxStreams.js
+++ rxStreams.js
@@ -1,20 +1,9 @@
 var Rx = require('rxjs/Rx');
 var assert = require('assert');
 
-
-
 function random() {
   return ((Math.floor(Math.random() * 100))%100 + 100) %100;
 }
-
-
-
-var source = Rx.Observable.of('Repeat this!').delay(500).repeat(60).map(random);
-var sources = [];
-for (var i=0; i < 3; i++) {
-  sources.push(createSource());
-}
-
 
 function createSource() {
   return Rx.Observable.of('Repeat this!').delay(5).repeat(60).map(random);
@@ -46,15 +35,21 @@ function newObserver() {
 
 var observer = newObserver();
 
-var subjects = sources.map(s => {var sub = new Rx.Subject(); s.subscribe(sub); return sub; });
+// Create 3 subjects and wire each of them to a random source
+var subjects = [1,2,3].map(s => {var sub = new Rx.Subject(); createSource().subscribe(sub); return sub; });
 
+// make the observers subscribe to subjects
 var observers = subjects.map(s => { var observer = newObserver() ;s.subscribe(observer); return observer; });
 
+// observer which will listen to all events
 var totalObserver = newObserver();
+
+// subject which will merge map all 3 subjects
 var totalSubject = Rx.Observable.of(0,1,2).mergeMap(x => subjects[x])
+
 totalSubject.subscribe(totalObserver);
 
-
+// test case to verify that the 3 individual observers receive same objects as the total observer
 totalSubject.subscribe({complete: () => {
   var sum = observers.map(o => o.arr).reduce((merged, o) => merged.concat(o), []).sort((x , y) => x - y);
   console.log('expected joint array    :' + sum);
